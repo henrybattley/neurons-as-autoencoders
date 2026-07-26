579b6f6701c775dd169fa3507a6516354c99d170
henrybattley <henrybattley@gmail.com>
2026-07-26 15:33:23 +0100

    adding sparsity penalty

---
+++ src/models/sparse_nan_cnn.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn  # neural network modules
+import torch.nn.functional as F  # useful stateless functions
+
+"""defines each filter (kernel) with the function of encoding and decoding its input"""
+class SimpleWeightShareConvFilter(nn.Module):
+    
+    def __init__(self, kernel_size=3,stride=1,padding=1,output_padding=0,bias=False,sigmoid=True,lambda_sparse=0.2):
+
+        super().__init__()
+
+        # encoder 
+        self.encoder = nn.Conv2d(
+            in_channels=1,
+            out_channels=1,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias, #False by default
+        )
+
+        #encoder He initialiasion for pre relu gates 
+        nn.init.kaiming_normal_(
+                                self.encoder.weight,
+                                mode="fan_out",
+                                nonlinearity="relu"
+        )
+
+        if bias==True:
+
+            nn.init.zeros_(self.encoder.bias)
+            #decoder weights reuse the encoder's, however decoder has a bias term
+            self.decoder_bias = nn.Parameter(torch.zeros(1))
+        else:
+            self.decoder_bias=None
+
+        self.stride = stride
+        self.padding = padding
+        self.output_padding = output_padding
+        self.sigmoid=sigmoid
+        self.lambda_sparse=lambda_sparse
+
+
+        #modern standard activation within convolutional networks is relu
+        self.activation = nn.ReLU()
+
+    #encode input (used by individual filters)
+    def encode(self, x):
+
+        h = self.activation(self.encoder(x))
+
+        return h
+
+        #encode input (used by individual filters)
+    def sparse_encode(self, x):
+
+
+        h = self.activation(self.encoder(x))     
+
+        sparsity_loss = self.lambda_sparse * h.abs().mean()
+
+
+        return h,sparsity_loss
+    
+    #calls encode and decode the latent feature representation (used by individual filters)
+    
+    def forward(self, x):
+
+        h,sparsity_loss = self.sparse_encode(x)
+
+        # do the transpose convolution but using the encoder weights
+        x_hat = F.conv_transpose2d(
+        h,
+        weight=self.encoder.weight,
+        bias=self.decoder_bias,
+        stride=self.stride,
+        padding=self.padding,
+        output_padding=self.output_padding
+    )
+
+        #maybe we don't even sigmoid here? to simplify
+        if not self.sigmoid:
+            return x_hat
+
+        return torch.sigmoid(x_hat),sparsity_loss
+
+
+    
+"""defines the network of ConvFilters"""
+class FilterCNN(nn.Module):
+
+    def __init__(
+            self, 
+            input_dims:int, 
+            kernel_size:int, 
+            stride: int, 
+            padding:int, 
+            n_filters:int,
+            classes:int, 
+            pool_kernel_size:int, 
+            pool_stride:int,
+            output_padding:int,
+            bias:bool,
+            sigmoid:bool,
+            lambda_sparse:float
+        ):
+        
+        super().__init__()
+
+        self.input_dims = input_dims
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding =padding
+        self.n_filters = n_filters
+        self.classes = classes
+        self.pool_kernel_size=pool_kernel_size
+        self.pool_stride = pool_stride
+        self.output_padding =output_padding
+        self.bias =bias
+        self.sigmoid=sigmoid
+        self.lambda_sparse=lambda_sparse
+        
+        #define the list of autoencoder filter submodules 
+        self.filters = nn.ModuleList([SimpleWeightShareConvFilter(kernel_size=kernel_size,stride=stride,padding=padding,output_padding=output_padding,bias=bias,sigmoid=sigmoid,lambda_sparse=lambda_sparse)for _ in range(n_filters)])
+
+        #self.pool = nn.MaxPool2d(pool_kernel_size,pool_stride)
+
+        #only works with square input..
+        conv_dim = ((input_dims + 2*padding - kernel_size) // stride) + 1             
+
+        #pool_dim = ((conv_dim - pool_kernel_size) // pool_stride) + 1                
+
+        self.fc = nn.Linear(n_filters * conv_dim * conv_dim, classes)
+
+        #xavier init for linear fully connected
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.zeros_(self.fc.bias)
+
+
+    # local reconstruction of one filter
+    def reconstruct(self, x, filter_idx):
+
+        #refers to the individual model forward function which does the reconstruction
+        return self.filters[filter_idx](x)
+    
+    
+    def extract_features(self, x):
+
+        feature_maps = [f.encode(x) for f in self.filters]
+        features = torch.cat(feature_maps, dim=1)
+        #features = self.pool(features)
+        features = features.flatten(1)
+        return features
+    
+
+    def classify(self, features):
+        return self.fc(features)
+
+
+    def forward(self, x):
+        return self.classify(self.extract_features(x))
